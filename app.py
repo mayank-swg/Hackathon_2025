70804acc902aa2f837ca28082c38578cec5ee81a
Mayank Sharma <mayank.sharma@swiggy.in>
2025-03-07 10:04:53 +0530

    refactor

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ session = requests.Session()
 session.cookies.clear()
 
 def search_amazon_product_ASIN(product_name):
-    url = f'{SCRAPINGBEE_API}?url=https://www.amazon.in/s?k={product_name.replace(' ', '+')}&ai_query={PRODUCT_ASIN_AI_QUERY.replace(' ', '+')}&api_key={SCRAPINGBEE_API_KEY}&block_ads={BLOCK_ADS}'
+    url = f'{SCRAPINGBEE_API}?url=https://www.amazon.in/s?k={product_name.replace(" ", "+")}&ai_query={PRODUCT_ASIN_AI_QUERY.replace(" ", "+")}&api_key={SCRAPINGBEE_API_KEY}&block_ads={BLOCK_ADS}'
     response = session.get(url, verify=False)
     if response.status_code == 200:
         print("asin_url", url)
@@ -31,7 +31,7 @@ def search_amazon_product_ASIN(product_name):
     return None
 
 def search_amazon_product_details(ASIN):
-    url = f'{SCRAPINGBEE_API}?url=https://www.amazon.in/dp/{ASIN}&ai_query={PRODUCT_DETAILS_AI_QUERY.replace(' ', '+')}&api_key={SCRAPINGBEE_API_KEY}&block_ads={BLOCK_ADS}'
+    url = f'{SCRAPINGBEE_API}?url=https://www.amazon.in/dp/{ASIN}&ai_query={PRODUCT_DETAILS_AI_QUERY.replace(" ", "+")}&api_key={SCRAPINGBEE_API_KEY}&block_ads={BLOCK_ADS}'
     response = session.get(url, verify=False)
     if response.status_code == 200:
         print("product_url", url)
